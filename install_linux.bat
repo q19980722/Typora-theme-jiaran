811cb863f9a6e90a55b3695fc108fc169fe8e475
root <root@vultr.guest>
2023-03-14 18:22:01 +0000

    rm scrips

--- install_linux.bat
+++
@@ -1,3 +0,0 @@
-cp jiaran.css  ~/.config/Typora/themes/jiaran.css
-cp jiaran-card.css ~/.config/Typora/themes/jiaran-card.css
-cp -r jiaran   ~/.config/Typora/themes/jiaran
--- install_windows.ps1
+++
@@ -1,3 +0,0 @@
-cp jiaran.css  ~\AppData\Roaming\Typora\themes
-cp jiaran-card.css ~\AppData\Roaming\Typora\themes
-cp -r jiaran   ~\AppData\Roaming\Typora\themes
